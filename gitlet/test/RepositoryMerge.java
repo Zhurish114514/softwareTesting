528cd898beeefde395b427a42ca803ff786c180a
zhu <375770568@qq.com>
2025-04-13 21:15:36 +0800

    修改

--- gitlet/test/RepositoryMerge.java
+++ gitlet/test/RepositoryMerge.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class RepositoryMerge {
     @Test
     public void testMergeConflict() {
+        // for normal occasions, commit with conflict should be solved correctly
         // Clean up existing repository
         if (Repository.GITLET_DIR.exists()) {
             if (!TestUtils.deleteDirectory(Repository.GITLET_DIR)) {
@@ -55,6 +57,8 @@ public class RepositoryMerge {
 
     @Test
     public void testSplitPoint() {
+        // the commits have two parents, the original commit and the C1 commit
+        // if the code works, the f.txt file should be removed
         if (Repository.GITLET_DIR.exists()) {
             if (!TestUtils.deleteDirectory(Repository.GITLET_DIR)) {
                 fail("Failed to delete existing .gitlet directory.");
@@ -63,36 +67,31 @@ public class RepositoryMerge {
 
         // Initialize repository
         Main.main(new String[]{"init"});
-
-        // Create initial file (split point)
-        TestUtils.createFile("wug.txt", "initial content");
-        Main.main(new String[]{"add", "wug.txt"});
-        Main.main(new String[]{"commit", "Initial commit"});
-
-        // Create branch B1 and B2
-        Main.main(new String[]{"branch", "B1"});
-        Main.main(new String[]{"branch", "B2"});
-
-        // Checkout B1 and add h.txt
-        Main.main(new String[]{"checkout", "B1"});
-        TestUtils.createFile("h.txt", "wug.txt");
-        Main.main(new String[]{"add", "h.txt"});
+        Main.main(new String[] {"branch", "B1"});
+        Main.main(new String[] {"branch", "B2"});
+        Main.main(new String[] {"checkout", "B1"});
+        TestUtils.createFile("hello.txt", "Hello, Gitlet!");
+        Main.main(new String[]{"add", "hello.txt"});
         Main.main(new String[]{"commit", "Add h.txt"});
-
-        // Checkout B2 and add f.txt
-        Main.main(new String[]{"checkout", "B2"});
-        TestUtils.createFile("f.txt", "wug.txt");
+        Main.main(new String[] {"checkout", "B2"});
+        TestUtils.createFile("f.txt", "Hello, Gitlet!");
         Main.main(new String[]{"add", "f.txt"});
         Main.main(new String[]{"commit", "f.txt added"});
-
-        // Create branch C1 from B2
-        Main.main(new String[]{"branch", "C1"});
-
-        // Modify B2: add g.txt and remove f.txt
-        TestUtils.createFile("g.txt", "notwug.txt");
+        Main.main(new String[] {"branch", "C1"});
+        TestUtils.createFile("g.txt", "Gitlet!");
         Main.main(new String[]{"add", "g.txt"});
         Main.main(new String[]{"rm", "f.txt"});
         Main.main(new String[]{"commit", "g.txt added, f.txt removed"});
+        Main.main(new String[] {"checkout", "B1"});
+        Main.main(new String[]{"merge", "C1"});
         Main.main(new String[]{"merge", "B2"});
+
+        String contentG = String.valueOf(TestUtils.readFile("g.txt"));
+        String contentH = String.valueOf(TestUtils.readFile("hello.txt"));
+        // f.txt should not exist
+
+        assertFalse(TestUtils.fileExists("f.txt"));
+        assertTrue(contentG.contains("Gitlet!"));
+        assertTrue(contentH.contains("Hello, Gitlet!"));
     }
 }
